d99c497da3487f856170cbd7973d16ba1c443312
Andrew Huntington <andrew.huntington@gmail.com>
2022-07-11 01:52:01 +0900

    Add background scrolling

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -4,6 +4,8 @@ import SceneKeys from "~/consts/SceneKeys";
 import AnimationKeys from "~/consts/AnimationKeys";
 
 export default class Game extends Phaser.Scene {
+  private background!: Phaser.GameObjects.TileSprite;
+
   constructor() {
     super(SceneKeys.Game);
   }
@@ -13,17 +15,37 @@ export default class Game extends Phaser.Scene {
     const width = this.scale.width;
     const height = this.scale.height;
 
-    this.add
+    this.background = this.add
       .tileSprite(0, 0, width, height, TextureKeys.Background)
-      .setOrigin(0, 0);
+      .setOrigin(0, 0)
+      .setScrollFactor(0, 0);
 
-    this.add
+    const mouse = this.physics.add
       .sprite(
         width * 0.5,
-        height * 0.5,
+        height - 30, // set y to top of the floor
         TextureKeys.RocketMouse,
         "rocketmouse_fly01.png"
       )
+      .setOrigin(0.5, 1) // set origin to feet
       .play(AnimationKeys.RocketMouseRun);
+
+    const body = mouse.body as Phaser.Physics.Arcade.Body;
+    body.setCollideWorldBounds(true);
+
+    body.setVelocityX(200);
+
+    this.physics.world.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height - 30);
+
+    this.cameras.main.startFollow(mouse);
+    this.cameras.main.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height);
+  }
+
+  /**
+   * @param  {number} t  - The total time elapsed since the game started.
+   * @param  {number} dt - The time elapsed since the last update.
+   */
+  update(t: number, dt: number) {
+    this.background.setTilePosition(this.cameras.main.scrollX);
   }
 }
